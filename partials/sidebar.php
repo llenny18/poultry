0f12e08bb03779284b47680d8d00605ea7584ae5
llenny18 <ramosalleneid01@gmail.com>
2023-11-25 12:45:45 +0800

    partials

---
+++ partials/sidebar.php
@@ -0,0 +1,34 @@
+<div class="sidebar-menu">
+            <div class="sidebar-header">
+                <div class="logo">
+                    <a href="index.php"><h2 style="color: white; padding: revert; margin: revert;">POULTRY FARM</h2></a>
+                </div>
+            </div>
+            <div class="main-menu">
+                <div class="menu-inner">
+                    <nav>
+                        <ul class="metismenu" id="menu">
+                            <li class="active">
+                                <a href="javascript:void(0)" aria-expanded="true"><i class="ti-dashboard"></i><span>dashboard</span></a>
+                                <ul class="collapse">
+                                    <li class="active"><a href="index.php">Home</a></li>
+                                    <li><a href="report.php">Reports</a></li>
+                                </ul>
+                            </li>
+                           
+                            <li>
+                                <a href="javascript:void(0)" aria-expanded="true"><i class="fa fa-table"></i>
+                                    <span>Cash Flow</span></a>
+                                <ul class="collapse">
+                                    <li><a href="feeds.php">Feeds</a></li>
+                                    <li><a href="misc.php">Miscellaneous</a></li>
+                                    <li><a href="invest.php">Investments</a></li>
+                                </ul>
+                            </li>
+                            <li><a href="record.php"><i class="ti-receipt"></i> <span>Records of Profits</span></a></li>
+                       
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+        </div>
