99ba5106c9f3496ec0c0771ee0ed49f23ced016b
Allen Eidrian Silva Ramos <115854514+llenny18@users.noreply.github.com>
2023-12-10 07:20:34 +0800

    Update poultryfarm-final.sql

--- db/poultryfarm-final.sql
+++ db/poultryfarm-final.sql
@@ -111,9 +111,9 @@ CREATE DEFINER=`root`@`localhost` PROCEDURE `update_pigSold` (IN `p_soldID` INT,
         soldID = p_soldID;
 END$$
 
-DELIMITER ;
-
+DELIMITER $$
 CREATE PROCEDURE `sell_pigs`(IN `list_id` VARCHAR(255)) BEGIN INSERT INTO `pigsold` (`soldID`, `soldCount`, `priceID`, `houseID`) VALUES (NULL, (select piglist.PigCount from piglist where piglist.pigListID = list_id), (select pigprice.priceID from pigprice order by pigprice.priceDate desc limit 1), (select piglist.HouseID from piglist where piglist.pigListID = list_id)); delete from piglist where piglist.pigListID = list_id; END;
+END$$
 
 -- --------------------------------------------------------
 
